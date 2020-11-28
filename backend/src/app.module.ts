366d474cae8995da5781efaacb027585117d746e
Vinicius Gomes de Oliveira <vinicius.oliveira@intelbras.com.br>
2020-11-27 22:00:30 -0300

    Aplicando Logger na aplicação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,25 @@ import { typeOrmConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { CategoriasModule } from './categorias/categorias.module';
 import { AuthModule } from './auth/auth.module';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './configs/winston.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, CategoriasModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig), 
+    WinstonModule.forRoot(winstonConfig), 
+    UsersModule, 
+    CategoriasModule, 
+    AuthModule
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
