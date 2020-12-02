e589f0ed4c28305b6f182e7cb2f12372cba83994
Vinicius Gomes de Oliveira <vinicius.oliveira@intelbras.com.br>
2020-12-01 23:05:02 -0300

    Aplicando envio de email, recuperação e confitmação

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,30 +1,30 @@
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, ExtractJwt } from 'passport-jwt';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UserRepository } from '../users/users.repository';
-
-@Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    @InjectRepository(UserRepository)
-    private userRepository: UserRepository,
-  ) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'd14cc536fea3798e37a5b3d9ee227eee',
-    });
-  }
-
-  async validate(payload: { id: number }) {
-    const { id } = payload;
-    const user = await this.userRepository.findOne(id, {
-      select: ['name', 'email', 'status', 'role'],
-    });
-    if (!user) {
-      throw new UnauthorizedException('Usuário não encontrado');
+  import { PassportStrategy } from '@nestjs/passport';
+  import { Strategy, ExtractJwt } from 'passport-jwt';
+  import { Injectable, UnauthorizedException } from '@nestjs/common';
+  import { InjectRepository } from '@nestjs/typeorm';
+  import { UserRepository } from '../users/users.repository';
+  
+  @Injectable()
+  export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+      @InjectRepository(UserRepository)
+      private userRepository: UserRepository,
+    ) {
+      super({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: 'd14cc536fea3798e37a5b3d9ee227eee',
+      });
     }
-
-    return user;
+  
+    async validate(payload: { id: number }) {
+      const { id } = payload;
+      const user = await this.userRepository.findOne(id, {
+        select: ['name', 'email', 'status', 'role', 'id'],
+      });
+      if (!user) {
+        throw new UnauthorizedException('Usuário não encontrado');
+      }
+  
+      return user;
+    }
   }
-}
